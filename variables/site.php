01ca9378dee3bb55473feafdbfcd004b93e99f63
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-11 03:10:48 -0800

    add variables lesson initial commit

---
+++ variables/site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+</head>
+<body>
+    echo "There once was a man name George <br>";
+    echo "He was 70 years old <br>";
+    echo "He really liked the name George <br>";
+    echo "But didn't like being 70 <br>";
+</body>
+</html>
