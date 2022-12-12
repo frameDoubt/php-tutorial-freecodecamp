4231981bbe93efb854eb08975ca80bc6f77b87a8
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-12 08:46:04 -0800

    add get-user-input lesson

--- getting-user-input/README.md
+++ getting-user-input/README.md
@@ -1,3 +1,8 @@
 # Sixth Lesson - Getting User Input
 ---
+Form html requires action attribute with the value of current document file name, and a method value of get.
 
+```
+$_GET["name-value-of-input"]
+```
+This is a super global variable, it used to collect data from a form after it is submited.
--- getting-user-input/site.php
+++ getting-user-input/site.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Getting User Input</title>
-</head>
-<body>
-    <?php
-        
-    ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Getting User Input</title>
+    </head>
+    <body>
+        <form action="site.php" method="get">
+            First Input: <input type="text" name="first">
+            <br>
+            Second Input: <input type="text" name="second">
+            <br>
+            <input type="submit">
+        </form>
+        <br>
+        The first output: <?php echo $_GET["first"] ?>
+        <br>
+        The second output: <?php echo $_GET["second"] ?>
+    </body>
 </html>
