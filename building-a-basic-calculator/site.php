041e272fc924be4783956f99ed29a856f9b971e4
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-13 03:48:08 -0800

    add basic calculator that returns the sum of two numbers

--- building-a-basic-calculator/site.php
+++ building-a-basic-calculator/site.php
@@ -7,6 +7,13 @@
     <title>Building A Basic Calculator</title>
 </head>
 <body>
-    <?php ?>
+    <form actions="site.php" method="get">
+        <input type="number" name="num1">
+        <br>
+        <input type="number" name="num2">
+        <input type="submit">
+    </form>
+
+    Answer: <?php echo $_GET["num1"] + $_GET["num2"]; ?>
 </body>
 </html>
