93fb256eb024bafb81a57b5c02a76d7c2bbceb3b
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-15 03:33:21 -0800

    add using-checkboxes lesson

--- using-checkboxes/README.md
+++ using-checkboxes/README.md
@@ -1,2 +1,3 @@
 # Twelfth Lesson - Using Checkboxes
 ---
+When using a list of items use name attribute followed square brackets, to signify the use of an array.
--- using-checkboxes/site.php
+++ using-checkboxes/site.php
@@ -7,11 +7,15 @@
     <title>Using Checkboxes</title>
 </head>
 <body>
-    <form action="" method="get">
-        
+    <form action="site.php" method="post">
+        Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+        Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+        Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+        <input type="submit">
     </form>
     <?php
-
+        $fruits = $_POST["fruits"];
+        echo $fruits[0];
     ?>
 </body>
 </html>
