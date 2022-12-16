e68359ded1f8b04bd4fb24b5a723b1078c888c31
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-16 06:53:34 -0800

    add if-statements-continued lesson

--- if-statements-continued/README.md
+++ if-statements-continued/README.md
@@ -1,2 +1,3 @@
 # Seventeenth Lesson - If Statements Continued
 ---
+A lesson on equality and comparison operators with if statements.
--- if-statements-continued/site.php
+++ if-statements-continued/site.php
@@ -7,6 +7,17 @@
     <title>If Statements Continued</title>
 </head>
 <body>
-    <?php ?>
+    <?php 
+        function getMax($num1, $num2, $num3){
+            if($num1 >= $num2 && $num1 >= $num3) {
+                return $num1;
+            } elseif($num2 >= $num1 && $num2 >= $num3) {
+                return $num2;
+            } else {
+                return $num3;
+            }
+        }
+        echo getMax(3000, 3000, 400);
+    ?>
 </body>
 </html>
