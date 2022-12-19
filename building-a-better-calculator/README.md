9c1d8177ebe15ec7709a9b1f280411359ce0fc46
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-18 20:12:53 -0800

    add building-a-better-calculator lesson

--- building-a-better-calculator/README.md
+++ building-a-better-calculator/README.md
@@ -1,2 +1,3 @@
 # Eighteenth Lesson - Building A Better Calculator
 ---
+HTML input type "number" defaults to integers, to change this use the step attribute to tell input what decimal point it can except.
--- building-a-better-calculator/site.php
+++ building-a-better-calculator/site.php
@@ -7,6 +7,27 @@
     <title>Build A Better Calculator</title>
 </head>
 <body>
-    <?php ?>
+    <form action="site.php" method="post">
+        First Number: <input type="number" step="0.1" name="num1"><br>
+        Operator: <input type="text" name="op"><br>
+        Second Number: <input type="number" name="num2"><br>
+        <input type="submit">
+    </form>
+    <?php
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $operator = $_POST["op"];
+        if($operator == "+"){
+            echo $num1 + $num2;
+        } elseif($operator == "-") {
+            echo $num1 - $num2;
+        } elseif($operator == "/") {
+            echo $num1 / $num2;
+        } elseif($operator == "*") {
+            echo $num1 * $num2;
+        } else {
+            echo "Invalid Operator";
+        }
+    ?>
 </body>
 </html>
