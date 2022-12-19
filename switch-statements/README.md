46143fa3b7255e6918fb5b2d5fad4b9a79df069d
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-18 20:44:16 -0800

    add switch-statements lesson

--- switch-statements/README.md
+++ switch-statements/README.md
@@ -1,2 +1,3 @@
 # Nineteenth Lesson - Switch Statements
 ---
+A break statement is required to get out of a case statement, otherwise it will run each statement until it hits a break statement. Use a default case to catch anything that doesn't have a case statement.
--- switch-statements/site.php
+++ switch-statements/site.php
@@ -7,8 +7,33 @@
     <title>Switch Statements</title>
 </head>
 <body>
+    <form action="site.php" method="post">
+        What was your grade? <br><br>
+        <input type="text" name="grade"><br><br>
+        <input type="submit">
+    </form>
+    <br>
     <?php
-        
+        $grade = $_POST["grade"];
+        switch($grade) {
+            case "A":
+                echo "You did amazing";
+                break;
+            case "B":
+                echo "You did alright";
+                break;
+            case "C":
+                echo "You can do better next time";
+                break;
+            case "D":
+                echo "You did very bad";
+                break;
+            case "F":
+                echo "You failed, oh no!";
+                break;
+            default:
+                echo "Invalid Grade";
+        }
     ?>
 </body>
 </html>
