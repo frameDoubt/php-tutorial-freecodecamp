4f06f9db7cb2489a96189161e71250151363c772
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-20 00:42:54 -0800

    add contructors lesson

--- constructors/README.md
+++ constructors/README.md
@@ -1,2 +1,3 @@
 # Twenty-Sixth - Constructors
 ---
+Contructors are a special type of function that is inside a class, it's used to give objects intial information. Constructor allow you to set values to objects while instantiating the object.
--- constructors/site.php
+++ constructors/site.php
@@ -8,7 +8,27 @@
 </head>
 <body>
     <?php
-    
+        class Book{
+        var $title;
+        var $author;
+        var $pages;
+        function __construct($aTitle, $aAuthor, $aPages)
+            {
+                $this->title = $aTitle;
+                $this->author = $aAuthor;
+                $this->pages = $aPages;
+            }
+        }
+
+        $book1 = new Book("Harry Potter", "JK Rowling", 400);
+
+        $book2 = new Book("Lord of the Rings", "Tolkien", 700);
+
+        echo $book1->title;
+        echo "<br> $book1->author";
+        echo "<br>";
+        echo $book2->title;
+        echo "<br> $book2->author";
     ?>
 </body>
 </html>
