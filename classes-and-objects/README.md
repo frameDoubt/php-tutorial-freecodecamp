560eca578e052f0e5dfe36a2fde6588ca9239258
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-19 23:20:06 -0800

    add classes-and-objects lesson

--- classes-and-objects/README.md
+++ classes-and-objects/README.md
@@ -1,2 +1,3 @@
 # Twenty-Fifth Lesson - Classes & Objects
 ---
+A class is essentially a specification/blueprint/template for a custom data type, it allows us to model something in the real world. Objects are an instance of a class.
--- classes-and-objects/site.php
+++ classes-and-objects/site.php
@@ -7,6 +7,28 @@
     <title>Classes & Objects</title>
 </head>
 <body>
-    <?php ?>
+    <?php
+        class Book{
+            var $title;
+            var $author;
+            var $pages;
+        }
+
+        $book1 = new Book;
+        $book1->title = "Harry Potter";
+        $book1->author = "JK Rowling";
+        $book1->pages = 400;
+
+        $book2 = new Book;
+        $book2->title = "Lord of the Rings";
+        $book2->author = "Tolkien";
+        $book2->pages = 700;
+
+        echo $book1->title;
+        echo "<br> $book1->author";
+        echo "<br>";
+        echo $book2->title;
+        echo "<br> $book2->author";
+    ?>
 </body>
 </html>
