04d1629912503e171f84a20db722f1bd656119e8
Brandon Wright <brandon.w.wright4@gmail.com>
2022-12-19 21:40:48 -0800

    add comments lesson

---
+++ comments/README.md
@@ -0,0 +1,3 @@
+# Twenty-Second Lesson - Comments
+---
+Comments are a line of code that doesn't get rendered by php compiler. There are single line comments and block comments. It uses the same comment markers as JavaScript, I suppose it might be more correct to say JavaScript uses the same markers as PHP.
---
+++ comments/site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments</title>
+</head>
+<body>
+    <?php
+        // This is a single line comment, it won't rendered.
+        /*
+            This is a comment block, it can be used to make multi-line comment.
+        */
+        echo "This is a statement that will be rendered by php compiler."
+    ?>
+</body>
+</html>
